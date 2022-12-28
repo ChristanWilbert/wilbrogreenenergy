d68fe2a386867f9fcd85617f4d31d3f42cc51c45
Christan Wilbert <cwchristan@gmail.com>
2022-12-28 12:03:17 +0530

    Deployment 1.1 Version 1.4

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,4 @@
-export const baseUrl = 'https://backend-wilbro.herokuapp.com/';
+//export const baseUrl = 'https://backend-wilbro.herokuapp.com/';
 export const siteUrl = "https://wilbro-green-energy.netlify.app/";
+//export const baseUrl='https://github.com/ChristanWilbert2003/ChristanWilbert2003.github.io/';
+export const baseUrl = 'backend-master-production.up.railway.app/';
