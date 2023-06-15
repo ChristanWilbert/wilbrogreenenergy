d7bdae63190bd531e914dbee38d801e811e59dea
Christan Wilbert <cwchristan@gmail.com>
2023-06-15 18:36:21 +0530

    Version 2.1 Deployment 2.1

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,121 +1,168 @@
-import React,{Component} from "react";
-import {Navbar, NavbarBrand,Nav,NavbarToggler,Collapse,NavItem,
-    Jumbotron,Button,Modal,ModalHeader,ModalBody, Form, FormGroup, Label, Input} from 'reactstrap';
-import{NavLink} from 'react-router-dom';
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
-class Header extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            isNavOpen:false,
-            isModalOpen: false,
-            username:'',
-            password:'',
-        };
-        this.toggleNav=this.toggleNav.bind(this);
-        this.toggleModal =this.toggleModal.bind(this);
-        this.handleLogin =this.handleLogin.bind(this);
-        }
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false,
+      username: "",
+      password: "",
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-    toggleNav(){
-        this.setState({isNavOpen:!this.state.isNavOpen});
-    }
-    toggleModal(){
-        this.setState({isModalOpen:!this.state.isModalOpen});
-    }
-    
-    handleLogin(event){
-        this.toggleModal();
-        alert("username: " + this.username.value + "Password: " + this.password.value+
-        "Remember" + this.remember.checked);
-        event.preventDefault();
+  toggleNav() {
+    this.setState({ isNavOpen: !this.state.isNavOpen });
+  }
+  toggleModal() {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  }
 
-
-    }
-    render(){
-        return(
-            <>
-            <Navbar dark expand="md">
-            <div className="container">
-                <NavbarToggler onClick={this.toggleNav}/>
-                <NavbarBrand className="mr-auto" href='/'>
-                  <img src="logo.png" height='40' width="50" alt="Right"></img>
-                </NavbarBrand>
+  handleLogin(event) {
+    this.toggleModal();
+    alert(
+      "username: " +
+        this.username.value +
+        "Password: " +
+        this.password.value +
+        "Remember" +
+        this.remember.checked
+    );
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <>
+        <Navbar dark expand="md">
+          <div className="container">
+            <NavbarToggler onClick={this.toggleNav} />
+            <NavbarBrand className="mr-auto" href="/">
+              <img src="logo.png" height="40" width="50" alt="Right"></img>
+            </NavbarBrand>
             <Collapse isOpen={this.state.isNavOpen} navbar>
-                <Nav navbar>
-                    <NavItem >
-                        <NavLink className="nav-link" to="/home" >
-                        <span className="fa fa-home fa-lg"></span> Home
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/aboutus" onClick={window.scrollBy(0,350)}>
-                        <span className="fa fa-info fa-lg"></span> About Us
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/menu">
-                        <span className="fa fa-list fa-lg"></span> Products
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/contactus">
-                        <span className="fa fa-address-card fa-lg"></span> Contact Us
-                        </NavLink>
-                    </NavItem>
-            </Nav>
-            <Nav className="ml-auto" navbar>
+              <Nav navbar>
                 <NavItem>
-                    <Button outline onClick={this.toggleModal}>
-                        <span className="fa fa-sign-in fa-lg"></span>
-                        Login
-                    </Button>
+                  <NavLink className="ml-3 nav-link" to="/home">
+                    <span className="fa fa-home fa-lg"></span> Home
+                  </NavLink>
                 </NavItem>
-            </Nav>
+                <NavItem>
+                  <NavLink
+                    className="ml-3 nav-link"
+                    to="/aboutus"
+                    onClick={window.scrollBy(0, 350)}
+                  >
+                    <span className="fa fa-info fa-lg"></span> About Us
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="ml-3 nav-link" to="/menu">
+                    <span className="fa fa-list fa-lg"></span> Products
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="ml-3 nav-link" to="/contactus">
+                    <span className="fa fa-address-card fa-lg"></span> Contact
+                    Us
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg"></span>
+                    Login
+                  </Button>
+                </NavItem>
+              </Nav>
             </Collapse>
+          </div>
+        </Navbar>
+        <Jumbotron>
+          <div className="container">
+            <div className="row row-header">
+              <div
+                className="col-md-12 m-1"
+                style={{ backgroundColor: "rgba(0,0,0,.6)" }}
+              >
+                <h1 className="text-center">
+                  <img
+                    src="logo.png"
+                    height="150"
+                    width="150"
+                    alt="center"
+                    style={{ padding: "10px 10px" }}
+                  />
+                  <br />
+                  WILBRO GREEN ENERGY
+                </h1>
+                <p className="text-center">Let's go green</p>
+              </div>
             </div>
-            </Navbar>
-            <Jumbotron>
-                <div className="container">
-                    <div className="row row-header">
-                        <div className="col-md-12 m-1" style={{backgroundColor: "rgba(0,0,0,.6)"}}>
-
-                            <h1 className="text-center">
-                                <img src="logo.png" height='150' width="150" alt="center" style={{padding:"10px 10px"}}/>
-                                <br/>WILBRO GREEN ENERGY</h1>
-                            <p className="text-center">Let's go green</p>
-                            </div>
-                    </div>
-
-                </div>
-            </Jumbotron>
-            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
-                <ModalBody>
-                    <Form onSubmit={this.handleLogin}>
-                        <FormGroup>
-                            <Label htmlFor="username">Username</Label>
-                            <Input type="text" id="username" name="username" innerRef={(input)=> this.username =input}/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label htmlFor="password">Password</Label>
-                            <Input type="password" id="password" name="password" innerRef={(input)=> this.password =input}/>
-                        </FormGroup>
-                        <FormGroup check>
-                            <Label check>
-                                <Input type="checkbox" name="remember" innerRef={(input)=> this.remember =input}/>
-                                Remember me
-                            </Label>
-                        </FormGroup>
-                        <Button type="submit" value="submit" color="primary">Login
-                        </Button>
-                    </Form>
-                </ModalBody>
-            </Modal>
-
-            </>
-        );
-    }
+          </div>
+        </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  type="text"
+                  id="username"
+                  name="username"
+                  innerRef={(input) => (this.username = input)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  innerRef={(input) => (this.password = input)}
+                />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="checkbox"
+                    name="remember"
+                    innerRef={(input) => (this.remember = input)}
+                  />
+                  Remember me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">
+                Login
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </>
+    );
+  }
 }
 
 export default Header;
--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,4 +1,4 @@
 //export const baseUrl = 'https://backend-wilbro.herokuapp.com/';
 export const siteUrl = "https://wilbro-green-energy.netlify.app/";
-//export const baseUrl='https://github.com/ChristanWilbert2003/ChristanWilbert2003.github.io/';
-export const baseUrl = 'backend-master-production.up.railway.app/';
+//export const siteUrl = "http://localhost:3000/";
+export const baseUrl = "https://backend-master-production.up.railway.app/";
